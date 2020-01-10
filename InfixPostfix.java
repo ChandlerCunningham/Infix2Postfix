0a8e1052c8ef88628842c7cbfcb359c9c73aba9e
Chandler Cunningham <59713235+ChandlerCunningham@users.noreply.github.com>
2020-01-09 18:15:24 -0600

    Create InfixPostfix.java

---
+++ InfixPostfix.java
@@ -0,0 +1,17 @@
+public class InfixPostfix 
+{
+
+	/**
+	 * Repeatedly evaluates input infix and postfix expressions.  See the project description
+	 * for the input description. It constructs a HashMap object for each expression and passes it 
+	 * to the created InfixExpression or PostfixExpression object. 
+	 *  
+	 * @param args
+	 **/
+	public static void main(String[] args) 
+	{
+		// TODO  
+	}
+	
+	// helper methods if needed
+}
